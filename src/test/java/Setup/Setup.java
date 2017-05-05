d12b6e21192391eed109ff45619165d333a838b6
Gustavo Rodrigues <grodrigu@thoughtworks.com>
2017-05-05 03:36:34 -0300

    Updates to maximinizer brower window configuration

--- src/test/java/Setup/Setup.java
+++ src/test/java/Setup/Setup.java
@@ -8,6 +8,7 @@ import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -34,7 +35,9 @@ public class Setup {
     public void setUp(){
 
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         driver.get(BasePage.BASE_URL);
@@ -44,7 +47,7 @@ public class Setup {
 
     @After
     public void tearDown(){
-        get(BasePage.BASE_URL +SEED_ENDPOINT).then().statusCode(200);
+        get(BasePage.BASE_URL + SEED_ENDPOINT).then().statusCode(200);
         driver.quit();
     }
 }
